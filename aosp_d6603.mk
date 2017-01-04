d6a3e351411dfc4fa28484e2e8fa09cf68a4faef
HaoZeke <rohit@myarchbox.com>
2017-01-04 10:34:08 +0530

    Dry Build

--- aosp_d6603.mk
+++ aosp_d6603.mk
@@ -13,14 +13,76 @@
 # limitations under the License.
 
 TARGET_KERNEL_CONFIG := aosp_shinano_leo_defconfig
+TARGET_KERNEL_SOURCE := kernel/sony/msm_leo
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/leo/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, vendor/google/products/gms.mk)
+$(call inherit-product, vendor/addons/config.mk)
+
+# Include custom telephony configuration
+include vendor/pixN/configs/custom_phone.mk
 
 PRODUCT_NAME := aosp_d6603
 PRODUCT_DEVICE := leo
 PRODUCT_MODEL := Xperia Z3 (AOSP)
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
+
+# Additional apps
+PRODUCT_PACKAGES += \
+    masquerade \
+    AOSPLinks \
+    VanillaMusic \
+    KernelAdiutor \
+    OmniGears \
+    Busybox 
+
+# DU Utils Library
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
+
+# DU Utils Library
+PRODUCT_PACKAGES += \
+org.dirtyunicorns.utils
+
+# Google Inclusions
+GAPPS_INCLUDED_PACKAGES += \
+    Keep \
+    NewsWidget
+
+# Google Exclusions
+GAPPS_EXCLUDED_PACKAGES += \
+    Hangouts \
+    YouTube \
+    Photos  \
+    Music
+
+# AICP packages
+#PRODUCT_PACKAGES += \
+AicpExtras 
+
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
+# TWRP Recovery
+DEVICE_RESOLUTION := 1440x814
+TW_THEME := portrait_hdpi
+TW_IGNORE_ABS_MT_TRACKING_ID := true
+
+# F2FS Support
+PRODUCT_PACKAGES += \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs
+
+# QCOM Flag
+QCOM_HARDWARE := true
+
+# DEXPREOPT
+WITH_DEXPREOPT := true
